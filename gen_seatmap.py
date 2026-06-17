6cf8654a23e96f8f8cd8a46daeb35629a1bd1109
heegeun84-ai <heegeun84@gmail.com>
2026-06-17 14:01:12 +0900

    배치도 스크린·1~6 테이블 간격 확보

--- gen_seatmap.py
+++ gen_seatmap.py
@@ -21,7 +21,7 @@ def ctext(s,cx,cy,fs,col,th):
 # --- 격자 좌표 (지그재그/벽돌식: 행마다 반 칸 오프셋) ---
 xfull=[0.065+0.84*i/12 for i in range(13)]
 off=0.035
-ys=np.linspace(0.225,0.93,6)
+ys=np.linspace(0.26,0.95,6)
 pos={}; num=1
 for ri in range(6):
     if ri==0:
@@ -33,13 +33,13 @@ for ri in range(6):
         pos[num]=(x,ys[ri]); num+=1
 
 # --- 무대(중앙, 실제 14.4m 비율로 넓게) + 양쪽 스크린(떨어져서 크게) ---
-sy1,sy2=int(.04*H),int(.145*H)
+sy1,sy2=int(.02*H),int(.12*H)
 sx1,sx2=int(.345*W),int(.655*W)
 cv2.rectangle(cv,(sx1,sy1),(sx2,sy2),(232,232,232),-1)
 cv2.rectangle(cv,(sx1,sy1),(sx2,sy2),(135,135,135),2)
 ctext("STAGE",.5*W,(sy1+sy2)/2,1.15,(110,110,110),2)
-cv2.rectangle(cv,(int(.483*W),sy2),(int(.517*W),int(.185*H)),(70,70,205),-1)  # 레드카펫
-syc1,syc2=int(.05*H),int(.175*H)
+cv2.rectangle(cv,(int(.483*W),sy2),(int(.517*W),int(.16*H)),(70,70,205),-1)  # 레드카펫
+syc1,syc2=int(.025*H),int(.13*H)
 for x1,x2 in [(.03*W,.20*W),(.80*W,.97*W)]:
     cv2.rectangle(cv,(int(x1),syc1),(int(x2),syc2),(244,234,214),-1)
     cv2.rectangle(cv,(int(x1),syc1),(int(x2),syc2),(170,130,70),2)
